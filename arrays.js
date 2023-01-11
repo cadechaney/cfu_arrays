4c67697a07b57a011613bbc7185a3cbfd84bdfef
Cade Chaney <cadechaney@live.com>
2023-01-11 17:13:12 -0600

    Add methods

--- arrays.js
+++ arrays.js
@@ -1,7 +1,11 @@
 
 
 var names = ["Max", "Matt", "Mickey", "Molly"];
-
+names.pop();
+// Removes the last element from an array and returns it. If the array is empty, undefined is returned and the array is not modified.
 var ages = [24, 62, 9, 40];
-
-var oldPeople [true, false, true, false, true]
+ages.push;
+// Push adds a new item to the end of an array
+var oldPeople [true, false, true, false, true];
+oldPeople.shift
+// Shift removes the element at the zeroth index and shifts the values at consecutive indexes down, then returns the removed value
